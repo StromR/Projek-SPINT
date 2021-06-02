a1af531743bf31810d55d4f20f7952f351896f4f
Naufal Geraldi <naufalgeraldi@gmail.com>
2021-06-02 19:49:07 +0700

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -183,7 +183,7 @@
               <hr>              
               <div class="row">
                 <div class="col-sm-3">
-                  <h6 class="mb-0">Game</h6>
+                  <h6 class="mb-0">MMR</h6>
                 </div>
                 <div class="col-sm-9 text-primary">
                   <?php echo $mmr ?>
